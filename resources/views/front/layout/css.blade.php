caec78e071fa0126c1fe34383e70143467dd3b4c
wisnuhidayat <wisnubaldas@gmail.com>
2023-03-20 07:09:36 +0700

    add favicon

--- resources/views/front/layout/css.blade.php
+++ resources/views/front/layout/css.blade.php
@@ -8,7 +8,11 @@
 <title>Sekolah Al-Amanah</title>
 
 <!-- ========== Favicon Icon ========== -->
-<link rel="shortcut icon" href="/assets/img/favicon.png" type="image/x-icon">
+{{-- <link rel="shortcut icon" href="/assets/img/favicon.png" type="image/x-icon"> --}}
+<link rel="apple-touch-icon" sizes="180x180" href="/assets/img/favicon/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="/assets/img/favicon/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="/assets/img/favicon/favicon-16x16.png">
+<link rel="manifest" href="/assets/img/favicon/site.webmanifest">
 
 <!-- ========== Start Stylesheet ========== -->
 <link href="/assets/css/bootstrap.min.css" rel="stylesheet" />
