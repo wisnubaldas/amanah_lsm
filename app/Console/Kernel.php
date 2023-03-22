1d2ce5aa0439a623dc2ee2c80d6ec84d562836fd
wisnuhidayat <wisnubaldas@gmail.com>
2023-03-22 19:11:08 +0700

    cron

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,6 +13,23 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         // $schedule->command('inspire')->hourly();
+        $path = base_path();
+
+        $schedule->call(function() use($path) {
+            if (file_exists($path . '/queue.pid')) {
+                $pid = file_get_contents($path . '/queue.pid');
+                $result = exec("ps -p $pid --no-heading | awk '{print $1}'");
+                $run = $result == '' ? true : false;
+            } else {
+                $run = true;
+            }
+
+            if($run) {
+                $command = '/usr/bin/php -c ' . $path .'/php.ini ' . $path . '/artisan queue:listen --tries=3 > /dev/null & echo $!';
+                $number = exec($command);
+                file_put_contents($path . '/queue.pid', $number);
+            }
+        })->name('monitor_queue_listener')->everyFiveMinutes();
     }
 
     /**
