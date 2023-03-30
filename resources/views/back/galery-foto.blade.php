438667ee712d8e8c9b2a438e522738417158106f
Wisnu Hidayat <wisnubaldas@gmail.com>
2023-03-31 00:13:56 +0700

    add bootstrap select

--- resources/views/back/galery-foto.blade.php
+++ resources/views/back/galery-foto.blade.php
@@ -23,44 +23,8 @@
                             fgroup-class="col-md-6" disable-feedback/>
                     </div>
                     <div class="col-4">
-                        @php
-    $config = [
-        "title" => "Select multiple options...",
-        "liveSearch" => true,
-        "liveSearchPlaceholder" => "Search...",
-        "showTick" => true,
-        "actionsBox" => true,
-    ];
-@endphp
-                <x-adminlte-select-bs id="selBsCategory" name="selBsCategory[]" label="Categories"
-                label-class="text-danger" igroup-size="sm" :config="$config" multiple>
-                <x-slot name="prependSlot">
-                    <div class="input-group-text bg-gradient-red">
-                        <i class="fas fa-tag"></i>
-                    </div>
-                </x-slot>
-                <x-slot name="appendSlot">
-                    <x-adminlte-button theme="outline-dark" label="Clear" icon="fas fa-lg fa-ban text-danger"/>
-                </x-slot>
-                <option data-icon="fa fa-fw fa-running text-info" data-subtext="Running">Sports</option>
-                <option data-icon="fa fa-fw fa-futbol text-info" data-subtext="Futbol">Sports</option>
-                <option data-icon="fa fa-fw fa-newspaper text-danger">News</option>
-                <option data-icon="fa fa-fw fa-gamepad text-warning">Games</option>
-                <option data-icon="fa fa-fw fa-flask text-primary">Science</option>
-                <option data-icon="fa fa-fw fa-calculator text-dark">Maths</option>
-            </x-adminlte-select-bs>
-            </div>
-            <!-- /.card-footer -->
-          </div>
-          <!-- /.card -->
-                    </div>
-                </div>
             </div>
             <!-- /.card-body -->
-            <div class="card-footer">
-                
-          
-        
     </div>
 @stop
 
